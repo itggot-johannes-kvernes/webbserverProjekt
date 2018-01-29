0dc902a021d9b95f7fb2db3259144a4ab6dcb175
itggot-johannes-kvernes <johannes.kvernes@itggot.se>
2018-01-29 16:03:18 +0100

    Changed the order the posts show up to "newest first"

--- models/Post.rb
+++ models/Post.rb
@@ -6,7 +6,7 @@ class Post
 
     def self.start_page_posts(user_id, app)
         db = SQLite3::Database.open('db/db.sqlite')
-        posts = db.execute('SELECT * FROM posts WHERE user_id IN (SELECT user1_id FROM friendships WHERE user2_id IS ?) OR user_id IN (SELECT user2_id FROM friendships WHERE user1_id IS ?)', [user_id, user_id])
+        posts = db.execute('SELECT * FROM posts WHERE user_id IN (SELECT user1_id FROM friendships WHERE user2_id IS ?) OR user_id IN (SELECT user2_id FROM friendships WHERE user1_id IS ?)', [user_id, user_id]).reverse
         return posts
     end
 
