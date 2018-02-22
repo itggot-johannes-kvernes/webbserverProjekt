835c0375969e33e6b1aa500c39756dce0ec49818
itggot-johannes-kvernes <johannes.kvernes@itggot.se>
2018-02-22 10:11:55 +0100

    Created method for user to join group

--- models/Group.rb
+++ models/Group.rb
@@ -14,4 +14,11 @@ class Group
         db.execute('INSERT INTO groups (name) VALUES (?)', name)
         app.redirect "/users/#{app.session[:user_id]}"
     end
+
+    def self.join(user_id, group_name, app)
+        db = SQLite3::Database.open('db/db.sqlite')
+        db.execute('INSERT INTO memberships (group_id, user_id) VALUES ((SELECT id FROM groups WHERE name IS ?), ?)', [group_name, user_id])
+        app.redirect "/users/#{app.session[:user_id]}"
+    end
+
 end
